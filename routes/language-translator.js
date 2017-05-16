eb20646dfdfd27dec3fae54eb6c4076067c32c72
uwitzig <urs_witzig@ch.ibm.com>
2017-05-16 16:52:46 +0200

    Update language-translator.js

--- routes/language-translator.js
+++ routes/language-translator.js
@@ -1,16 +1,20 @@
 'use strict';
 
 var watson = require('watson-developer-cloud');
+var config = require('../config');
+
 
 var languageTranslator = watson.language_translator({
-  url: 'https://gateway.watsonplatform.net/language-translator/api',
-  version: 'v2'
+    url: config.watson.language_translator.url,
+    username: config.watson.language_translator.username,
+    password: config.watson.language_translator.password,
+    version: config.watson.language_translator.version
 });
 
 module.exports.translate = function(req, res, next) {
   var params = {
     text: req.body.text,
-    model_id: 'en-es'
+    model_id: 'en-de'
   };
   languageTranslator.translate(params, function(error, result) {
     if (error)
